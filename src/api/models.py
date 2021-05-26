c94306ab7f8ca117a6f9b1e6b8f58cb47977f020
sergioadll <sergio.adll@hotmail.com>
2021-05-26 19:53:32 +0000

    first commit

--- src/api/models.py
+++ src/api/models.py
@@ -27,7 +27,6 @@ class User(db.Model):
             "id": self.id,
             "username": self.username,
             "email": self.email,
-            "user_houses": user_houses,
             # do not serialize the password, its a security breach
         }
 
